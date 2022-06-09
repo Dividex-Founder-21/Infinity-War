cd37f36c59e8e94b18b737269664761a1ba515d6
Nick_Castel <101661926+Dividex-Founder-21@users.noreply.github.com>
2022-06-08 19:52:54 -0700

    Add files via upload

--- DividexPair.sol
+++ DividexPair.sol
@@ -1,13 +1,13 @@
 pragma solidity = 0.8.14;
 
 import './interfaces/IDividexPair.sol';
-import './interfaces/IDividexERC20.sol';
 import './interfaces/IDividexCallee.sol';
-import './interfaces/IERC20.sol';
-import './DividexERC20.sol';
+import './ERC1155.sol';
+import './Interfaces/IERC1155.sol';
+import './Interfaces/IERC20.sol';
 import './libraries/Math.sol';
 
-contract DividexPair is IDividexPair, IDividexERC20 {
+contract DividexPair is IDividexPair, ERC1155{
 
     uint public constant MINIMUM_LIQUIDITY = 10**3;
     bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
@@ -15,6 +15,12 @@ contract DividexPair is IDividexPair, IDividexERC20 {
     address public factory;
     address public token0;
     address public token1;
+    //ERC1155 indexes
+    uint public constant indexToken0 = 0;
+    uint public constant indexToken1 = 1;
+    uint public constant indexLPToken0 = 2;
+    uint public constant indexLPToken1 = 3;
+
     uint32 public baseFee;
 
     uint112 private reserve0;           // uses single storage slot, accessible via getPoolVariables
@@ -29,8 +35,9 @@ contract DividexPair is IDividexPair, IDividexERC20 {
     uint112 private liability0; //new variable for Dividex calculations
     uint112 private liability1; //new variable for Dividex calculations
 
-    constructor() public {
+    constructor() {
         factory = msg.sender;
+
     }
 
     uint private unlocked = 1;
@@ -64,7 +71,7 @@ contract DividexPair is IDividexPair, IDividexERC20 {
         baseFee = _baseFee;
     }
 
-    function _safeTransfer(address token, address to, uint value) private {
+    function _safeTransfer(address token, address to, uint value) private {//this function is made obselete by safeBatchTransfer
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'Dividex: TRANSFER_FAILED');
     }
@@ -78,8 +85,8 @@ contract DividexPair is IDividexPair, IDividexERC20 {
 
     function _updateSlipDif(uint balance0, uint balance1, uint112 _range0, uint112 _range1,
      uint112 _liability0, uint112 _liability1) private {
-        balanceModified0 = balance0*(10**9);
-        balanceModified1 = balance1*(10**9);
+        uint balanceModified0 = balance0*(10**9);
+        uint balanceModified1 = balance1*(10**9);
         slipDif0 = uint112(balanceModified0) > (liability0 - range0) ? uint112(balanceModified0) - (liability0 - range0) : 0;
         slipDif1 = uint112(balanceModified1) > (liability1 - range1) ? uint112(balanceModified1) - (liability1 - range1) : 0;
     }
@@ -103,30 +110,30 @@ contract DividexPair is IDividexPair, IDividexERC20 {
         uint amount1 = balance1 - _reserve1;
 
         //uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update
-        
+
         //token 0 mint determination
         if (_liability0 == 0) {
             liquidity0 = amount0 - MINIMUM_LIQUIDITY;
-           _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
+           _mint(address(0), 2, MINIMUM_LIQUIDITY, ""); // permanently lock the first MINIMUM_LIQUIDITY tokens
         } else {
             liquidity0 = amount0;
         }
-        require(liquidity0 > 0 || liquidity1, 'Dividex: INSUFFICIENT_LIQUIDITY_MINTED');
+        _mint(to, 2, liquidity0, "");
 
         //token 1 mint determination
         if (_liability1 == 0) {
             liquidity1 = amount1 - MINIMUM_LIQUIDITY;
-           _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
+           _mint(address(0), 3, MINIMUM_LIQUIDITY,""); // permanently lock the first MINIMUM_LIQUIDITY tokens
         } else {
             liquidity1 = amount1;
         }
-        require(liquidity0 > 0 || liquidity1, 'Dividex: INSUFFICIENT_LIQUIDITY_MINTED');
-        _mint(to, liquidity0);
-        _mint(to, liquidity1);
-        //why do we not reupdate balances here like in burn
+        require(liquidity0 > 0 || liquidity1 >0, 'Dividex: INSUFFICIENT_LIQUIDITY_MINTED');
+        _mint(to, 3, liquidity0, "");
+        
+        //why do we not reupdate balances here like in burn - coins have already been added to the pool, look at uint amount0 and amount1
         _updateLiquidity(liquidity0, liquidity1, balance0, balance1); //update slipdif called in this function
         _update(balance0, balance1, _reserve0, _reserve1);
-        emit Mint(msg.sender, liqudity0, liquidity1);
+        emit Mint(msg.sender, liquidity0, liquidity1);
     }
 
     //will have to make significant changes by using ERC 1155
@@ -143,8 +150,8 @@ contract DividexPair is IDividexPair, IDividexERC20 {
         uint balance1 = 10**9*IERC20(_token1).balanceOf(address(this));
 
         //create if statements for liquidity
-        liquidity0 = balanceOf[address(this)]; //should work because liquidity0 made in mint which is external contract
-        liquidity1 = balanceOf[address(this)]; //should work because liquidity1 made in mint which is external contract
+        uint liquidity0 = balanceOf(address(this), 2);
+        uint liquidity1 = balanceOf(address(this), 3);
 
         if (balance0 <= _liability0 && balance1 <= _liability1) { //perfect balance or deficit (without surplus)
          // using balances ensures pro-rata distribution
@@ -167,10 +174,11 @@ contract DividexPair is IDividexPair, IDividexERC20 {
         }
 
         require(amount0 > 0 || amount1 > 0, 'Dividex: INSUFFICIENT_LIQUIDITY_BURNED'); //change to or statement
-        _burn(address(this), liquidity0);
-        _burn(address(this), liquidity1);
-        _safeTransfer(_token0, to, amount0);
-        _safeTransfer(_token1, to, amount1);
+        //_burnBatch(address(this), [2,3], [liquidity0, liquidity1]);
+        _burn(address(this), 2, liquidity0);
+        _burn(address(this), 3, liquidity0);
+        _safeTransfer(_token0, to, amount0); // optimistically transfer tokens
+        _safeTransfer(_token1, to, amount1); // optimistically transfer tokens
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
 
@@ -239,32 +247,11 @@ contract DividexPair is IDividexPair, IDividexERC20 {
             range = _range1;
         }
         require(fee > 0, 'Dividex: Invalid fee'); //must have earned some fee
-        uint base = (10**9)/2;
-        uint lpPerc = base + (10**9)*Math.sqrt(Math.min(Math.max(slipDif/Range),0)); //check to make sure each math operation doesn't truncate decimals
-        uint divPerc = 10**9 - lpPerc;
-        require(lpPerc + divPerc = 10**9, 'Dividex: Invalid calculation'); //must check out and add to 100%
-        lpReward = lpPerc * fee;
-        divReward = divPerc * fee;
+        //uint base = (10**9)/2;
+        lpReward = fee/2 + fee*Math.sqrt(Math.min(Math.max(slipDif/range,1),0)); //check to make sure each math operation doesn't truncate decimals
+        divReward = fee - lpReward;
+        require(fee == lpReward + divReward, 'Dividex: Invalid calculation'); //must check out and add to 100%
         //must emit event here - build function in interface - maybe not
-
-    }
-
-    function feeDidurse(address to, uint amountOut0, uint amountOut1, uint totalFeesOwed0, uint totalFeesOwed1) external lock {
-        require(amountOut0 > 0 || amountOut1 > 0, 'Invalid fee amount : Dividex');
-        require(amountOut0 < totalFeesOwed0 && amountOut1 < totalFeesOwed1, 'Fee more than owed: Dividex'); // check less than what is actually owed - will come in from website
-        address _token0 = token0;
-        address _token1 = token1; //are fees stored in balance? Where are fees stored?
-
-        //do we need balance? - unsure
-        //balance0 = IERC20(_token0).balanceOf(address(this));
-        //balance1 = IERC20(_token1).balanceOf(address(this));
-        //require(amount0Out < balance0 && amount1Out < balance1, 'Fee more than total balance: Dividex');
-
-        //_feeDisburse(address(this), amount0, liquidity1); //look into this, I am a little confused
-        _safeTransfer(_token0, to, amount0);
-        _safeTransfer(_token1, to, amount1);
-
-        emit feeDisburse(msg.sender, amount0Out, amount1Out, to);
 
     }
 
